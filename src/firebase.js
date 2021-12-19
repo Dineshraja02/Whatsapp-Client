43aca77efc067888e1cfd5664f03db561e8b633e
Dinesh Raja <dineshraja0403@gmail.com>
2021-12-19 11:15:51 +0530

    whatsapp clone

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCUv2m1rbR0qIObFCkKlNehQwT9loj3vBw",
+    authDomain: "whatsapp-clone-b3057.firebaseapp.com",
+    projectId: "whatsapp-clone-b3057",
+    storageBucket: "whatsapp-clone-b3057.appspot.com",
+    messagingSenderId: "296861256412",
+    appId: "1:296861256412:web:88d504ee80539e66e5c219"
+  };
